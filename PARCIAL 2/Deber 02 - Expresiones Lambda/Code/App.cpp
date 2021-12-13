85b0fd8ba73a7e8aa3b57ea5c39789d1332c5f58
Josué Merino <yoshichicharra11@gmail.com>
2021-12-12 22:35:30 -0500

    Codigo

--- PARCIAL 2/Deber 02 - Expresiones Lambda/Code/App.cpp
+++ PARCIAL 2/Deber 02 - Expresiones Lambda/Code/App.cpp
@@ -8,7 +8,7 @@
 	-Pablo Yanez							     ||
 	-Daniel Vizcarra							 ||
 	-Fecha de Creación: 11 / 12 / 2021			 ||
-	-Fecha de Modificación: 11 / 12 / 2021		 ||
+	-Fecha de Modificación: 12 / 12 / 2021		 ||
 //////////////////////////////////////////////////
 */
 
@@ -20,72 +20,36 @@
 
 using namespace std;
 
-void suma_lambda(float a, float b)
-{
-	float sum = [](float x, float y) -> float { return x + y ; }(a, b);
-}
-
-float resta_lambda(float a, float b) {
-	float res = [](float x, float y) -> float { return x - y; }(a, b);
+int lambda_suma_parametros(int a, int b) {
+	int res = [](int x,int y)->int {
+		std::cout << x + y << std::endl;
+		return 0;
+		}(a,b);
 	return res;
 }
 
-float multiplicacion_lambda(float a, float b) {
-	float res = [](float x, float y) -> float { return x * y; }(a, b);
-	return res;
-}
-
-float division_lambda(float a, float b) {
-	float res = [](float x, float y) -> float { return x / y; }(a, b);
+int lambda_suma_capturas(int a, int b) {
+	int x = a;
+	int y = b;
+	int res = [=]()->int {
+		std::cout << x + y << std::endl;
+		return 0;
+		}();
 	return res;
 }
 
 int main() {
 	setlocale(LC_ALL,"");
-	float a = 0.0f;
-	float b = 0.0f;
-	int option;
-	cout << "OPERACIONES BASICAS CON EXPRESIONES LAMBDA" << endl;
-	cout << "\n1.-Suma";
-	cout << "\n2.- Resta";
-	cout << "\n3.- Multiplicacion";
-	cout << "\n4.-Division";
-	cout << "\nQue Operacion desea realizar: ";
-	cin >> option;
-	cout << "\n";
-	switch (option) {
-	case 1:
-		cout << "\nIngrese un número: ";
-		cin >> a;
-		cout << "\nIngrese otro numero: ";
-		cin >> b;
-		suma_lambda(a, b);
-		break;
-	case 2:
-		cout << "\nIngrese un número: ";
-		cin >> a;
-		cout << "\nIngrese otro numero: ";
-		cin >> b;
-		resta_lambda(a,b);
-		break;
-	case 3:
-		cout << "\nIngrese un número: ";
-		cin >> a;
-		cout << "\nIngrese otro numero: ";
-		cin >> b;
-		multiplicacion_lambda(a, b);
-		break;
-	case 4:
-		cout << "\nIngrese un número: ";
-		cin >> a;
-		cout << "\nIngrese otro numero: ";
-		cin >> b;
-		division_lambda(a, b);
-		break;
-	default:
-		cout << "Ingrese una opcion valida!";
-		break;
-	}
-	return 0;
+	lambda_suma_parametros(5, 4);
+	lambda_suma_capturas(7, 6);
+
+	int x = 10;
+	int y = 5;
+	auto sum = [](auto a, auto b) {
+		std::cout << a + b << std::endl;
+	};
+	sum(-10, 10);
+	sum(0.7, 0.3);
+	sum(std::string("Hola "), std::string("Guapo"));
 }
 
